34b93b5dc2bf228609a67964da6b2cea6e86494d
turner-kl <angra5296@gmail.com>
2022-07-16 21:20:04 +0900

    initial commit

---
+++ photo-share-api/index.js
@@ -0,0 +1,29 @@
+const { ApolloServer } = require('apollo-server');
+
+const typeDefs = `
+    type Query {
+        totalPhotos: Int!
+    }
+    
+    type Mutation {
+        postPhoto(name: String! description: String): Boolean!
+    }
+`;
+
+const photos = [];
+const resolvers = {
+    Query: {
+        totalPhotos: () => photos.length
+    },
+    Mutation: {
+        postPhoto(parent, args) {
+            photos.push(args);
+            return true;
+        }
+    }
+
+}
+
+const server = new ApolloServer({ typeDefs, resolvers });
+
+server.listen().then(({ url }) => console.log(`GraphQL Service running on ${url}`));
