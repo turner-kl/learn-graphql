bef9341cd666f9888da782acf4379194b31ca7fe
turner-kl <angra5296@gmail.com>
2022-07-23 09:21:13 +0900

    query

--- photo-share-client/src/index.js
+++ photo-share-client/src/index.js
@@ -2,10 +2,16 @@ import React from 'react';
 import { render } from 'react-dom'
 import './index.css';
 import App from './App';
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from 'react-apollo';
+
+const client = new ApolloClient({ uri: 'http://localhost:4000/graphql' });
 
 render(
   <React.StrictMode>
-    <App />
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
